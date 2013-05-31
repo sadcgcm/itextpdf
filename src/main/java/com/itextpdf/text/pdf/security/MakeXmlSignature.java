a7d2903caa032c1cf4b707ec1b9ecfc54b2c77a3
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-05-31 09:29:15 +0000

    Adding license headers.

--- src/main/java/com/itextpdf/text/pdf/security/MakeXmlSignature.java
+++ src/main/java/com/itextpdf/text/pdf/security/MakeXmlSignature.java
@@ -1,14 +1,58 @@
+/*
+ * $Id$
+ *
+ * This file is part of the iText (R) project.
+ * Copyright (c) 1998-2012 1T3XT BVBA
+ * Authors: Pavel Alay, Bruno Lowagie, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY 1T3XT,
+ * 1T3XT DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA, or download the license from the following URL:
+ * http://itextpdf.com/terms-of-use/
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every PDF that is created
+ * or manipulated using iText.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the iText software without
+ * disclosing the source code of your own applications.
+ * These activities include: offering paid services to customers as an ASP,
+ * serving PDFs on the fly in a web application, shipping iText with a closed
+ * source product.
+ *
+ * For more information, please contact iText Software Corp. at this
+ * address: sales@itextpdf.com
+ */
 package com.itextpdf.text.pdf.security;
 
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.error_messages.MessageLocalization;
 import com.itextpdf.text.pdf.XmlSignatureAppearance;
-import org.apache.xml.security.utils.Base64;
-import org.apache.jcp.xml.dsig.internal.dom.DOMReference;
-import org.apache.jcp.xml.dsig.internal.dom.DOMSignedInfo;
-import org.apache.jcp.xml.dsig.internal.dom.DOMXMLSignature;
-import org.apache.jcp.xml.dsig.internal.dom.DOMUtils;
-import org.apache.jcp.xml.dsig.internal.dom.DOMKeyInfoFactory;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+import org.jcp.xml.dsig.internal.dom.DOMReference;
+import org.jcp.xml.dsig.internal.dom.DOMSignedInfo;
+import org.jcp.xml.dsig.internal.dom.DOMXMLSignature;
+import org.jcp.xml.dsig.internal.dom.DOMXMLSignatureFactory;
+import org.jcp.xml.dsig.internal.dom.DOMUtils;
+import org.jcp.xml.dsig.internal.dom.DOMKeyInfoFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -84,8 +128,7 @@ public class MakeXmlSignature {
         if (!externalSignature.getHashAlgorithm().equals("SHA1"))
             throw new UnsupportedOperationException(MessageLocalization.getComposedMessage("support.only.sha1.hash.algorithm"));
 
-        XMLSignatureFactory fac = XMLSignatureFactory.getInstance
-                        ("DOM", new org.apache.jcp.xml.dsig.internal.dom.XMLDSigRI());
+        XMLSignatureFactory fac = DOMXMLSignatureFactory.getInstance();
         DigestMethod digestMethodSHA1 = fac.newDigestMethod(DigestMethod.SHA1, null);
 
         Document doc = xmlLocator.getDocument();
@@ -95,11 +138,12 @@ public class MakeXmlSignature {
 
         // Create the Reference
         XpathConstructor xpathConstructor = sap.getXpathConstructor();
-        if (xpathConstructor != null && xpathConstructor.getXpathExpression().length() > 0) {
+        if (xpathConstructor != null && !xpathConstructor.getXpathExpression().isEmpty()) {
             XPathFilter2ParameterSpec xpath2Spec = new XPathFilter2ParameterSpec(Collections.singletonList(new XPathType(xpathConstructor.getXpathExpression(), XPathType.Filter.INTERSECT)));
             transforms.add(fac.newTransform(Transform.XPATH2, xpath2Spec));
         }
-        DOMReference reference = (DOMReference)fac.newReference("", digestMethodSHA1, transforms, null, null);
+        DOMReference reference = (DOMReference)fac.newReference("", digestMethodSHA1,
+                transforms, null, null);
 
         String signatureMethod;
         if (externalSignature.getEncryptionAlgorithm().equals("RSA"))
